e3585ba794dbbce161133ae54e7267017902af27
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-03-29 19:08:42 -0400

    Created main function

---
+++ f8/f8/f8.cpp
@@ -0,0 +1,12 @@
+/*
+* FILE : f8.cpp
+* PROJECT : SENG1000- focused assignment 8
+* PROGRAMMER : Bhawanjeet Kaur Gill
+* FIRST VERSION : 2024-03-29
+* DESCRIPTION : Write a program to give you practice at working with C-style strings, files, command-line arguments,
+*               and structs.
+*/
+int main(int argc, char* argv[])
+{
+	return 0;
+}
