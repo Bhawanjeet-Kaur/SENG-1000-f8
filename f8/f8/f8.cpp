fa79551c9ad4c3d6e9d5b5f81d712b292657dd5e
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-03-29 19:27:02 -0400

    adding data in myArgs

--- f8/f8/f8.cpp
+++ f8/f8/f8.cpp
@@ -6,7 +6,38 @@
 * DESCRIPTION : Write a program to give you practice at working with C-style strings, files, command-line arguments,
 *               and structs.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning(disable: 4996)
+
+#define kMaximumLength 21
+struct MyData
+{
+	int howMany = 0;
+	char theText[kMaximumLength] = "";
+	char directoryPath[kMaximumLength] = "";
+	char filename[kMaximumLength] = "";
+};
+
 int main(int argc, char* argv[])
 {
+	if (argc != 5)
+	{
+		printf("Error: The program requires four arguments only.\n");
+		return 1;
+	}
+	struct MyData myArgs;
+	myArgs.howMany = atoi(argv[1]);
+	strncpy(myArgs.theText, argv[2], kMaximumLength - 1);
+	myArgs.theText[kMaximumLength - 1] = '\0';
+	strncpy(myArgs.directoryPath, argv[3], kMaximumLength - 1);
+	myArgs.directoryPath[kMaximumLength - 1] = '\0';
+	strncpy(myArgs.filename, argv[4], kMaximumLength - 1);
+	myArgs.filename[kMaximumLength - 1] = '\0';
+
+	char fullPath[kMaximumLength];
+	snprintf(fullPath, sizeof(fullPath), "%s\%s", myArgs.directoryPath, myArgs.filename);
+
 	return 0;
 }
